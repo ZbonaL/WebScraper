16425cc9fd3dedfda60c974041f5df94dfbd94af
Leonard Zbona <leonard.zbona@uoit.net>
2019-07-19 14:07:18 -0400

    finished MONTH-%d-%Y conversion

--- scraper.py
+++ scraper.py
@@ -33,7 +33,20 @@ for i in headTables:
       if time:
         timeVal = dt.strptime(time.group(), "%B %d, %Y")
         event = re.sub(r"[ADFJMNOS]\w* [\d]{1,2}, [\d]{4}",str(timeVal), event)
-      # elif :
+      elif doubleTime:
+        dates = re.split(r"\ to\ |\ |,\ ", event)
+        startDates = " ".join([dates[0],dates[1],dates[3]])
+        endDates = " ".join([dates[0],dates[2],dates[3]])
+        
+        startTime = dt.strptime(startDates, "%B %d %Y")
+        # print(startTime)
+        endTime = dt.strptime(endDates, "%B %d %Y")
+        # print(endTime)
+        finalDates = ' to '.join([str(startTime), str(endTime)])
+        print(finalDates)
+        event = re.sub(r"[ADFJMNOS]\w* [\d]{1,2} to [\d]{1,2}, [\d]{4}",str(finalDates), event)
+        # print(event)
+
       list_of_cells.append(event)
     list_of_rows.append(list_of_cells)
   
